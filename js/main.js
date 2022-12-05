39f1ea9ceff7ca470fa5323e5d5bb681811dec03
Ciro Meluziis <meluziis.ciro99@gmail.com>
2022-12-05 18:27:40 +0100

    axios call

--- js/main.js
+++ js/main.js
@@ -4,10 +4,21 @@ const { createApp } = Vue;
 createApp({
     data() {
         return {
-
+            apiUrl: 'server.php',
+            albums: [],
+        }
+    },
+    methods: {
+        getAlbums() {
+            axios.get(this.apiUrl)
+                .then(res => {
+                    //console.log(res.data);
+                    this.albums = res.data;
+                    console.log(albums);
+                })
         }
     },
     mounted() {
-        console.log('ciao');
+        this.getAlbums();
     }
 }).mount('#app')
